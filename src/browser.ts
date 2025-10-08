02de778a10325b0c50fdefb1ce54cacd139d736f
Nikhil Sonti <nikhilsv92@gmail.com>
2025-10-08 09:48:48 -0700

    base commit

---
+++ src/browser.ts
@@ -0,0 +1,166 @@
+/**
+ * @license
+ * Copyright 2025 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
+
+import type {
+  Browser,
+  ChromeReleaseChannel,
+  ConnectOptions,
+  LaunchOptions,
+  Target,
+} from 'puppeteer-core';
+import puppeteer from 'puppeteer-core';
+
+let browser: Browser | undefined;
+
+const ignoredPrefixes = new Set([
+  'chrome://',
+  'chrome-extension://',
+  'chrome-untrusted://',
+  'devtools://',
+]);
+
+function targetFilter(target: Target): boolean {
+  if (target.url() === 'chrome://newtab/') {
+    return true;
+  }
+  for (const prefix of ignoredPrefixes) {
+    if (target.url().startsWith(prefix)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const connectOptions: ConnectOptions = {
+  targetFilter,
+};
+
+export async function ensureBrowserConnected(browserURL: string) {
+  if (browser?.connected) {
+    return browser;
+  }
+  browser = await puppeteer.connect({
+    ...connectOptions,
+    browserURL,
+    defaultViewport: null,
+  });
+  return browser;
+}
+
+interface McpLaunchOptions {
+  acceptInsecureCerts?: boolean;
+  executablePath?: string;
+  customDevTools?: string;
+  channel?: Channel;
+  userDataDir?: string;
+  headless: boolean;
+  isolated: boolean;
+  logFile?: fs.WriteStream;
+  viewport?: {
+    width: number;
+    height: number;
+  };
+  args?: string[];
+}
+
+export async function launch(options: McpLaunchOptions): Promise<Browser> {
+  const {channel, executablePath, customDevTools, headless, isolated} = options;
+  const profileDirName =
+    channel && channel !== 'stable'
+      ? `chrome-profile-${channel}`
+      : 'chrome-profile';
+
+  let userDataDir = options.userDataDir;
+  if (!isolated && !userDataDir) {
+    userDataDir = path.join(
+      os.homedir(),
+      '.cache',
+      'chrome-devtools-mcp',
+      profileDirName,
+    );
+    await fs.promises.mkdir(userDataDir, {
+      recursive: true,
+    });
+  }
+
+  const args: LaunchOptions['args'] = [
+    ...(options.args ?? []),
+    '--hide-crash-restore-bubble',
+  ];
+  if (customDevTools) {
+    args.push(`--custom-devtools-frontend=file://${customDevTools}`);
+  }
+  if (headless) {
+    args.push('--screen-info={3840x2160}');
+  }
+  let puppeteerChannel: ChromeReleaseChannel | undefined;
+  if (!executablePath) {
+    puppeteerChannel =
+      channel && channel !== 'stable'
+        ? (`chrome-${channel}` as ChromeReleaseChannel)
+        : 'chrome';
+  }
+
+  try {
+    const browser = await puppeteer.launch({
+      ...connectOptions,
+      channel: puppeteerChannel,
+      executablePath,
+      defaultViewport: null,
+      userDataDir,
+      pipe: true,
+      headless,
+      args,
+      acceptInsecureCerts: options.acceptInsecureCerts,
+    });
+    if (options.logFile) {
+      // FIXME: we are probably subscribing too late to catch startup logs. We
+      // should expose the process earlier or expose the getRecentLogs() getter.
+      browser.process()?.stderr?.pipe(options.logFile);
+      browser.process()?.stdout?.pipe(options.logFile);
+    }
+    if (options.viewport) {
+      const [page] = await browser.pages();
+      // @ts-expect-error internal API for now.
+      await page?.resize({
+        contentWidth: options.viewport.width,
+        contentHeight: options.viewport.height,
+      });
+    }
+    return browser;
+  } catch (error) {
+    if (
+      userDataDir &&
+      ((error as Error).message.includes('The browser is already running') ||
+        (error as Error).message.includes('Target closed') ||
+        (error as Error).message.includes('Connection closed'))
+    ) {
+      throw new Error(
+        `The browser is already running for ${userDataDir}. Use --isolated to run multiple browser instances.`,
+        {
+          cause: error,
+        },
+      );
+    }
+    throw error;
+  }
+}
+
+export async function ensureBrowserLaunched(
+  options: McpLaunchOptions,
+): Promise<Browser> {
+  if (browser?.connected) {
+    return browser;
+  }
+  browser = await launch(options);
+  return browser;
+}
+
+export type Channel = 'stable' | 'canary' | 'beta' | 'dev';
